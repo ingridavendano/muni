d50d9ec3fe4059a30eaa9fdfab8727e596b35a98
Ingrid Avendaño <ingridavendano@gmail.com>
2014-01-13 21:30:59 -0800

    Wrote encoder class.

--- serialize.py
+++ serialize.py
@@ -4,3 +4,35 @@
 # -----------------------------------------------------------------------------
 # Useful functions to write data into JSON.
 # -----------------------------------------------------------------------------
+
+import json
+
+class DepartureEncoder(json.JSONEncoder):
+    """ Encode XML departure times to JSON. """
+
+    def default(self, rtt, name):
+
+        def stop_data(stop):
+            return {
+                'route': stop.get("Name"),
+                'direction': stop[0][0].get('Name'), 
+                'times': [time.text for time in stop[0][0][0][0][0]]
+            }
+
+        return {
+            'stop': [stop_data(stop) for stop in rtt[0][0][0]]
+        }
+
+# -----------------------------------------------------------------------------
+
+def to_json(name, rtt, debug=False):
+    """ Converts data about a MUNI stop into JSON. """
+
+    # if no XML stop exists 
+    if name is None: return None
+
+    # encode XML  
+    json_string = json.dumps(rtt, cls=DepartureEncoder)
+
+    if debug: print json_string
+    return json_string
